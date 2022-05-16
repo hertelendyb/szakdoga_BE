eed3b96f6c58926f2fb15bdd1e857e86c691576c
Bálint Hertelendy <hertelendyb@gmail.com>
2022-05-16 11:15:50 +0200

    project routes linked with org

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -2,6 +2,8 @@ import {
   Body,
   Controller,
   Get,
+  Param,
+  ParseIntPipe,
   Post,
   Session,
   UseGuards,
@@ -12,19 +14,22 @@ import { CreateProjectDto } from './dtos/create-project.dto';
 import { ProjectsService } from './projects.service';
 
 @UseGuards(AuthGuard)
-@Controller('projects')
+@Controller('organizations/:id/projects')
 export class ProjectsController {
   constructor(private projectsService: ProjectsService) {}
 
   @Post('/create')
-  async createProject(@Body() body: CreateProjectDto, @Session() session: any) {
-    const project = await this.projectsService.create(body.name, session.orgId);
+  async createProject(
+    @Body() body: CreateProjectDto,
+    @Param('id', ParseIntPipe) orgId: number,
+  ) {
+    const project = await this.projectsService.create(body.name, orgId);
     return project;
   }
 
   @Get('/')
-  async listProjects(@Session() session: any) {
-    const myProjects = await this.projectsService.getMyProjects(session.orgId);
+  async listProjects(@Param('id', ParseIntPipe) orgId: number) {
+    const myProjects = await this.projectsService.getMyProjects(orgId);
     return myProjects;
   }
 
